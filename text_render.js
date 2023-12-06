a2373fdf5b2f79a70ef0a53d2b9941daaeedc9e8
AzverexD <54556836+lmoboy@users.noreply.github.com>
2023-12-06 11:02:07 +0200

    complete rework

--- text_render.js
+++ text_render.js
@@ -1,131 +1,262 @@
+async function getData(){
+    let promise = await fetch("https://api.github.com/users/lmoboy/repos");
+    return await promise.json();
 
-    var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-    
-    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-    
-    var TextScramble = function () {
-      function TextScramble(el) {
+}
+const repos = getData();
+console.log(repos)
+
+let _createClass = function () { function defineProperties(target, props) { for (let i = 0; i < props.length; i++) { let descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+let TextScramble = function () {
+    function TextScramble(el) {
         _classCallCheck(this, TextScramble);
-    
+
         this.el = el;
         this.chars = '!<>-_\\/[]{}@€”=+*^?#________';
         this.update = this.update.bind(this);
-      }
-    
-      _createClass(TextScramble, [{
+    }
+
+    _createClass(TextScramble, [{
         key: 'setText',
         value: function setText(newText) {
-          var _this = this;
-    
-          var oldText = this.el.innerText;
-          var length = Math.max(oldText.length, newText.length);
-          var promise = new Promise(function (resolve) {
-            return _this.resolve = resolve;
-          });
-          this.queue = [];
-          for (var i = 0; i < length; i++) {
-            var from = oldText[i] || '';
-            var to = newText[i] || '';
-            var start = Math.floor(Math.random() * 40);
-            var end = start + Math.floor(Math.random() * 40);
-            this.queue.push({ from: from, to: to, start: start, end: end });
-          }
-          cancelAnimationFrame(this.frameRequest);
-          this.frame = 0;
-          this.update();
-          return promise;
+            let _this = this;
+
+            let oldText = this.el.innerText;
+            let length = Math.max(oldText.length, newText.length);
+            let promise = new Promise(function (resolve) {
+                return _this.resolve = resolve;
+            });
+            this.queue = [];
+            for (let i = 0; i < length; i++) {
+                let from = oldText[i] || '';
+                let to = newText[i] || '';
+                let start = Math.floor(Math.random() * 40);
+                let end = start + Math.floor(Math.random() * 40);
+                this.queue.push({ from: from, to: to, start: start, end: end });
+            }
+            cancelAnimationFrame(this.frameRequest);
+            this.frame = 0;
+            this.update();
+            return promise;
         }
-      }, {
+    }, {
         key: 'update',
         value: function update() {
-          var output = '';
-          var complete = 0;
-          for (var i = 0, n = this.queue.length; i < n; i++) {
-            var _queue$i = this.queue[i],
-                from = _queue$i.from,
-                to = _queue$i.to,
-                start = _queue$i.start,
-                end = _queue$i.end,
-                char = _queue$i.char;
-    
-            if (this.frame >= end) {
-              complete++;
-              output += to;
-            } else if (this.frame >= start) {
-              if (!char || Math.random() < 0.28) {
-                char = this.randomChar();
-                this.queue[i].char = char;
-              }
-              output += '<span class="text">' + char + '</span>';
-            } else {
-              output += from;
+            let output = '';
+            let complete = 0;
+            for (let i = 0, n = this.queue.length; i < n; i++) {
+                let _queue$i = this.queue[i],
+                    from = _queue$i.from,
+                    to = _queue$i.to,
+                    start = _queue$i.start,
+                    end = _queue$i.end,
+                    char = _queue$i.char;
+
+                if (this.frame >= end) {
+                    complete++;
+                    output += to;
+                } else if (this.frame >= start) {
+                    if (!char || Math.random() < 0.28) {
+                        char = this.randomChar();
+                        this.queue[i].char = char;
+                    }
+                    output += '<span class="text">' + char + '</span>';
+                } else {
+                    output += from;
+                }
             }
-          }
-          this.el.innerHTML = output;
-          if (complete === this.queue.length) {
-            this.resolve();
-          } else {
-            this.frameRequest = requestAnimationFrame(this.update);
-            this.frame++;
-          }
+            this.el.innerHTML = output;
+            if (complete === this.queue.length) {
+                this.resolve();
+            } else {
+                this.frameRequest = requestAnimationFrame(this.update);
+                this.frame++;
+            }
         }
-      }, {
+    }, {
         key: 'randomChar',
         value: function randomChar() {
-          return this.chars[Math.floor(Math.random() * this.chars.length)];
+            return this.chars[Math.floor(Math.random() * this.chars.length)];
         }
-      }]);
-    
-      return TextScramble;
-    }();
-    
-    var phrases = [
-        "so basically, this is a thing",
-        "name is daniel",
-        "aka AzverexD",
-        "could've used something easier but ok",
-        "javascript kinda sucks",
-        "ASM or c++ is easier than web development",
-        "I would love to share something",
-        "I really like this animation",
-        "if you are wondering how this works",
-        "here is an explanation by ai",
-        "The `var phrases` variable is an array of strings that contains the phrases that will be displayed on the screen.",
-        "The `var el` variable is a reference to the div element with the class `text`.",
-        "The `var fx` variable is a reference to the `TextScramble` object that is used to animate the text.",
-        "The `var counter` variable is used to keep track of the current phrase in the `var phrases` array.",
-        "The `var next` function is used to call the `setText()` method on the `fx` object and then schedule a call to the `next()` function again after phrase length * 60.",
-        "The `var clicked` variable is used to keep track of whether or not the user has clicked on the screen.",
-        "The `document.onclick` event handler is used to call the `next()` function when the user clicks on the screen.",
-        "oh also this is on loop",
-        "if you want you can stay here for 10 min",
-        "song mix is 10 min so u good"
-    ];
-    
-    var el = document.querySelector('.text');
-    var fx = new TextScramble(el);
-    var timeout;
-    var counter = 0;
-    var next = function next() {
-        fx.setText(phrases[counter]).then(function () {
-            timeout = setTimeout(next, phrases[counter === 0 ? counter : counter - 1].length * 60);
-        });
-        counter = (counter + 1) % phrases.length;
-    };
+    }]);
 
-    var clicked = false;
-    if (!clicked){
-        fx.setText("click to start the animation (or to skip text)");
+    return TextScramble;
+}();
+
+
+let el       = document.querySelector('.text');
+let cont     = document.querySelector( '.container' );
+let fx   = new TextScramble(el);
+
+
+
+let blur = false;
+let clicked = false;
+if (!clicked){
+    fx.setText("click to load portfolio");
+}
+
+function copy_to_clipboard(name) {
+    navigator.clipboard.writeText(name);
+}
+
+document["onclick"] = () => {
+    if (clicked) {
+        return;
     }
-    document.onclick = () => {
-        if (clicked) {
-            clearTimeout(timeout)
-            next();
-            return;
-        }
-        clicked = true;
-        var aud = new Audio("music.mp3");
-        aud.loop = true;
-        aud.play();
-        next();
-    }
+
+    clicked = true;
+    let aud = new Audio("music.mp3");
+    aud.loop = true;
+    aud.play();
+    fx.setText("loading portfolio...");
+    setTimeout(function () {
+        el.classList.add("text-animation");
+        setTimeout(function () {
+            el.remove();
+            setTimeout(function () {
+
+
+
+
+                let portfolioContainer = document.createElement("div");
+
+
+                portfolioContainer.classList.add("portfolio-container");
+                cont.append(portfolioContainer);
+
+                    setTimeout(function () {
+                        let portfolioPictureContainer= document.createElement("div")
+                        portfolioPictureContainer.classList.add ("portfolio-picture-container");
+                        portfolioContainer.append(portfolioPictureContainer)
+                        let portfolioPicture = document.createElement("img")
+                        portfolioPicture.src = "https://forum.neverlose.cc/user_avatar/forum.neverlose.cc/azverexd/120/248465_2.png"
+                        portfolioPicture.classList.add("portfolio-image")
+                        portfolioPicture.style.cursor = "pointer"
+                        portfolioPictureContainer.append(portfolioPicture)
+
+                        let linkContainer  = document.createElement( 'div')
+                        let d           = document.createElement( 'a' )
+                        let g           = document.createElement( 'a' )
+                        let s           = document.createElement( 'a' )
+                        let t           = document.createElement( 'a');
+                        let link_hover = false;
+                        let pict_hover = false;
+
+                        portfolioPicture.addEventListener("mouseleave", function () {
+                            pict_hover = false
+
+                        }, false);
+                        portfolioPicture.addEventListener("mouseover", function () {
+                            pict_hover = true
+
+                        }, false);
+
+                        linkContainer.addEventListener("mouseleave", function () {
+                            link_hover = false
+
+                        }, false);
+                        linkContainer.addEventListener("mouseover", function () {
+                            link_hover = true
+
+                        }, false);
+
+
+
+
+                        portfolioPicture.addEventListener('click',function () {
+                            blur = !blur;
+
+
+                            if (blur) {
+                                portfolioPicture.style.cursor = "default";
+
+                                linkContainer.style.filter = "opacity(0)";
+                                linkContainer.style.transition = "all 0.2s ease";
+                                cont.append(linkContainer);
+
+                                linkContainer.classList.add("link-container");
+                                linkContainer.classList.add("link-container-animate");
+
+
+
+                                s.classList.add("portfolio-text")
+                                g.classList.add("portfolio-text")
+                                d.classList.add("portfolio-text")
+                                t.classList.add("portfolio-text")
+
+
+                                s.href = "https://steamcommunity.com/id/AzverexD/"
+                                g.href = "https://github.com/lmoboy"
+                                t.href = "https://t.me/AzverexD"
+
+
+                                d["onclick"]=()=>{
+                                    setTimeout(function (){
+                                        discord.setText("discord")
+                                    },2000)
+                                    discord.setText("copied")
+                                    copy_to_clipboard("smarrtie")
+                                }
+                                t.style.bottom = ("30%")
+                                s.style.bottom = ("40%")
+                                g.style.bottom = ("50%")
+                                d.style.bottom = ("60%")
+                                d.style.cursor = ("pointer")
+                                let discord     = new TextScramble(d)
+                                let github      = new TextScramble(g)
+                                let steam       = new TextScramble(s)
+                                let telegram    = new TextScramble(t)
+                                
+                                discord.setText("discord")
+                                github.setText("github")
+                                steam.setText("steam")
+                                telegram.setText("telegram")
+                                linkContainer.append(d, g, s, t)
+                                setTimeout(function (){
+                                    linkContainer.style.filter = "opacity(1)";
+                                },0)
+                            }else
+                            {
+                                    linkContainer.style.transition = "all 0.2s ease";
+                                    linkContainer.style.filter = "opacity(0)";
+
+                                    setTimeout(function (){
+                                        linkContainer.removeChild(d)
+                                        linkContainer.removeChild(g)
+                                        linkContainer.removeChild(s)
+                                        cont.removeChild(linkContainer)
+                                    }, 500)
+                            }
+
+
+                        })
+                        document["onclick"] = () => {
+                            if(!link_hover && !pict_hover && blur){
+                                blur = !blur;
+                                linkContainer.style.transition = "all 0.2s ease";
+                                linkContainer.style.filter = "opacity(0)";
+                                if(!linkContainer){return}
+                                setTimeout(function (){
+                                    linkContainer.removeChild(d)
+                                    linkContainer.removeChild(g)
+                                    linkContainer.removeChild(s)
+                                    cont.removeChild(linkContainer)
+                                }, 200)
+                            }
+                        }
+                        setTimeout(function () {
+                            let portfolioTitle = document.createElement("h3")
+                            let portfolioTitleAnim = new TextScramble(portfolioTitle);
+                            portfolioTitleAnim.setText("AzverexD");
+                            portfolioTitle.classList.add("portfolio-text");
+                            portfolioContainer.append(portfolioTitle)
+                        },0)
+                    }, 0)
+            }, 0)
+        }, 5000)
+    }, 1000)
+
+}
